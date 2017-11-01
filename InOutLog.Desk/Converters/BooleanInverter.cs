4cbb0c6ff81159033ef64d2743c1e31609fc4c38
ikopcanski <ivan.kopcanski@hotmail.com>
2017-11-02 00:25:15 +0400

    Splash screen 'Please wait' introduced.

---
+++ InOutLog.Desk/Converters/BooleanInverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace InOutLog.Desk
+{
+    public class BooleanInverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !((value is bool) && ((bool)value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
